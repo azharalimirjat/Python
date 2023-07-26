3855fb60aaf857f91a4674a4dfdb4cddefa38080
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-07-26 17:14:11 +0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,17 @@
+# This is comment in python for single line
+
+# This is how we print any statement
+print("Hello Azhar")
+
+# We can also write the code in single quotation mark
+print('Hello Azhar')
+# Both are right
+
+# After running both statements, you will see (Hello Azhar) typed on the console
+
+"""     => This is multiple line comments symbol
+INDENTATION:
+    In the written form of many languages, an indentation or indent is an empty space 
+at the beginning of a line to signal the start of a new paragraph. Many computer languages 
+have adopted this technique to designate "paragraphs" or other logical blocks in the program.
+"""
---
+++ Variable.py
@@ -0,0 +1,19 @@
+"""
+#VARIABLE
+    Python variables are simply containers for storing data values.
+Unlike other languages, such as Java, Python has no command for declaring a variable,
+so you create one the moment you first assign a value to it.
+Python variables are simply containers for storing data values.
+"""
+# 34234 =10    wrong* (No number)
+# 23x -10      wrong* (No number at the start)
+# !@##$%* = 10 wrong* (No special character)
+# print = 10   wrong* (No name after built-in function)
+#   x=10       wrong* (Indentation error)
+# na me =10    wrong* (no space b/w given name)
+
+x=10 # right
+print(x)   #console will show 10
+
+a , b = 10, 20 #right
+print(a,b)  #this will show 10 20 on console
