a77bc842034c2f5012596a48160f688acb81f9ac
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-07-26 17:39:46 +0500

    Add files via upload

---
+++ NumericData.py
@@ -0,0 +1,42 @@
+# NUMERIC DATA
+"""
+Numeric Data are of two types
+1. Integer:Numbers without decimal (ex: 22, 32, 34, 54545 etc)
+2. Float:  Numbers with decimal    (ex: 22.23, 32.43, 34.65 etc)
+
+"""
+
+
+# number =10 => (this is expression b/c of =)
+
+
+"""
+Basic mathematical operations:
+1. Addition (+) 
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division (/)
+5. Double Division (//)
+6. Modulo (%)
+"""
+
+a=10
+b=20
+
+# Addition
+# print("Addition is: "+a+b)
+
+# Subtraction
+print(a-b)
+
+# Multiplication
+print(a*b)
+
+# Division
+print(a/b)
+
+# Double Division
+print(a//b) #Gives whole number part as answer
+
+# Modulo
+print(a%b)
