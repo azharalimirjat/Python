ee2944f7125225e5286f756e4dbd0ad8aaf250ba
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-07-26 18:06:56 +0500

    Add files via upload

---
+++ ArithematicOperations.py
@@ -0,0 +1,41 @@
+"""
+Basic mathematical operations:
+1. Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Power/Exponent (**)
+5. Division (/)
+6. Double Division (//)
+7. Modulo (%)
+8. Parenthesis ( () )
+"""
+
+# Python follows PEDMAS Rule
+# PEDMAS= Parenthesis, Exponents, Division, Multiplication, Addition, Subtraction.
+
+a=3
+b=6
+
+# Addition
+# print("Addition is: "+a+b)
+
+# Subtraction
+print(a-b)
+
+# Multiplication
+print(a*b)
+
+# Exponent
+print(a**b)
+
+# Division: It returns floating value
+print(a/b)
+
+# Double Division
+print(a//b) #Gives whole number part as answer
+
+# Modulo
+print(a%b)
+
+# Parenthesis
+print(a+(b-a)+b)
