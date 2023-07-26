092c3f3d98f10737818ad3757105c6c54abfd1c1
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-07-26 06:29:51 -0700

    AugmentedAssignments.py

---
+++ AugmentedAssignments.py
@@ -0,0 +1,37 @@
+# AUGMENTED ASSIGNMENT
+a=10
+# print(a)
+# incrementing by any number
+
+# Example1 Addition:
+a=a+1;
+# print(a)
+# We can also write this as
+a+=1;
+# print(a)
+
+# Example2 Subtraction:
+num=5;
+num=num-2   # or    (num-=2) #Both are same
+
+# Example3 Multiplication:
+num=5;
+num=num*2   # or    (num*=2) #Both are same
+
+
+# Example4 Division:
+num=5;
+num=num/2   # or    (num/=2) #Both are same
+
+
+# Example5 Exponent:
+num=5;
+num=num**2  # or    (num**=2) #Both are same
+
+# Example6 Modulo:
+num=5;
+num=num%2 # or  (num%=2) #Both are same
+
+# Example2 Double Division:
+num=5;
+num=num//2   # or    (num//=2) #Both are same
