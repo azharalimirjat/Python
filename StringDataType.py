a38a37c8431fe4c861cce5cfdd76674e6aeba8ab
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-07-27 17:01:44 +0500

    Add files via upload

---
+++ StringDataType.py
@@ -0,0 +1,33 @@
+"""
+STRING DATA TYPE
+Strings are sequences of character data.
+The string type in Python is called str .
+"""
+
+# Defining String
+# variable_Name = "Value_Here"
+
+name= "Azhar Ali"
+print(name)
+
+
+"""
+Slice of String: 
+    Breaking sequence of characters(String value) in parts
+Ex: firstname = "Azhar Ali"
+print(name[0]) in capital brackets we give postion to show that value
+Output on console will be= A
+
+
+"""
+print(name[0]) #Output A
+print(name[1]) #Output z
+print(name[2]) #Output h
+print(name[3]) #Output a
+print(name[4]) #Output r
+print(name[5]) #Output nothing will show b/c at this position there is space
+print(name[6]) #Output A
+print(name[7]) #Output l
+print(name[8]) #Output i
+
+
