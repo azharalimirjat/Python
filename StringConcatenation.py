1e6a2c73568d96516b8d95c96f4ce6048b00117f
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-07-29 21:31:10 +0500

    Add files via upload

---
+++ StringConcatenation.py
@@ -0,0 +1,33 @@
+"""
+STRING CONCATENATION:
+    This means combining/Addition of two or more string together by using (+) sign.
+EX:
+
+"""
+firstname = "Azhar"
+lastname = "Ali"
+fullname = firstname+lastname
+print(fullname) #(Output will be AzharAli) => this is known as String Concatenation
+
+
+# Concatenation of two or more numbers used as numbers
+a = "1"
+b = "2"
+print(a+b) #Ouput will be ( "12" )
+
+
+# We can also use another String b/w addition of two or more Strings
+# EX:
+fname = "Azhar "
+lname = "Mirjat"
+print(fname+"Ali "+lname) #Ouput ( "Azhar Ali Mirjat" )
+
+# Note==> You have to define space by yourself otherwise Strings concatenated will be merged
+
+"""
+String Concatenation by Multiplying:
+    By multiplying any number with string you will get that number times the string
+EX"
+"""
+char = "x"
+print(char*10) #Output ( "xxxxxxxxxx" ) ten times 'x'
