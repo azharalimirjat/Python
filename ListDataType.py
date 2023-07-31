7cbddf157c3c615af578042850a69a0d6983c598
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-07-31 20:23:37 +0500

    Add files via upload

---
+++ ListDataType.py
@@ -0,0 +1,24 @@
+"""
+LIST DATA TYPE:
+     Lists are used to store multiple items in a single variable.
+Syntax:
+variable_name = [element1,element2,...,elementn]
+"""
+
+list = ["Azhar", "Ali"]
+print(list) #output: you will get whole list as it is written
+
+"""
+==> Accessing Particular value from list
+==> We use index/position of the element to access
+Syntax:
+print(list_name[index_of_element])
+==> Index starts from 0.
+
+Example below:
+"""
+citylist = ["Jamshoro","Larkana","Karachi","Sukkur","Hyderabad"]
+print(citylist)
+print(citylist[0]) #Output: Jamshoro
+
+print(citylist[3]) #output: Sukkur
