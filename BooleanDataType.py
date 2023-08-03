7a7d1d5b44251f336d21b62e388340b2c13f2dcc
Azhar Ali <99320098+Azhar-Ali-Mirjat@users.noreply.github.com>
2023-08-03 18:26:40 +0500

    Add files via upload

---
+++ BooleanDataType.py
@@ -0,0 +1,36 @@
+"""
+BOOLEAN DATA TYPE:
+    Python boolean type is one of the built-in data types provided by Python,
+    which represents one of the two values i.e. True or False.
+
+For example,
+    1==1 is True whereas
+    2<1 is False.
+
+Boolean Data Type Works on Comparison Operators:
+Below are comparison operators with examples:
+"""
+
+# 1. Equal to ( == )
+print(2==2) #Output: true
+print(2==4) #Output: false
+
+# 2. not equal to ( != )
+print(2!=2) #Output: false
+print(3!=2) #Output: true
+
+# 2. Greater than ( > )
+print(2>3) #Output: false
+print(3>2) #Output: true
+
+# 2. less than ( < )
+print(4<3) #Output: false
+print(2<3) #Output: true
+
+# 2. Greater or equal to  ( >= )
+print(2>=3) #Output: false
+print(3>=2) #Output: true
+
+# 2. less than ( > )
+print(2<=3) #Output: true
+print(10<=5) #Output: false
