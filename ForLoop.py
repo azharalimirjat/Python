ac4ca3b37266e4f108ee8df5115f3471899f8152
Azhar Ali <99320098+azharalimirjat@users.noreply.github.com>
2023-08-15 23:48:17 +0500

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,32 @@
+"""
+For Loop:
+    The for loop in Python is used to iterate over a sequence, which
+could be a list, tuple, array, or string.
+
+Examples:
+"""
+list = [1,2,3,4,5,6,7,8,9]
+
+for var_b in list:
+    print(var_b)
+    print(var_b)
+# it will print every number two times.
+
+list1 = [1,2,3,4,5]
+for var_a in list1:
+    print(f'The value is: {var_a}')
+# Using statement in loops, which will be printed everytime the loop iterates.
+
+
+# Using For Loop for range:
+for i in range(11):
+    print(i)
+# It will print eleven numbers starting from 0 to 10.
+
+
+# Creating String List
+namesList = ['Azhar','Shahid','Haseeb','Arayan','Fahad','Jawad','Qurban']
+for name in namesList:
+    print(name)
+# All names will be printed.
+
