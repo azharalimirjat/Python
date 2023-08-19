a67eab8ab1131c95eddf025fba4c23126631ba2d
Azhar Ali <99320098+azharalimirjat@users.noreply.github.com>
2023-08-19 13:56:24 +0500

    Add files via upload

---
+++ IfStatement.py
@@ -0,0 +1,25 @@
+"""
+Python supports the usual logical conditions from mathematics:
+
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+
+IF Statement:
+     one of the most commonly used conditional statements in programming languages.
+     It decides whether certain statements need to be executed or not. It checks for a given condition,
+     if the condition is true, then the set of code present inside the ” if ” block will be executed otherwise not.
+
+Syntax:
+if <condition>:
+    <expression>
+EX:
+"""
+x = 3
+y = 10
+
+if x < y:
+    print("x is smaller than y.") #OUTPUT: x is smaller than y.
