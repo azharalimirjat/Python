58181942d4c87a6b0d755e0eb2c7b05ed497c901
Azhar Ali <99320098+azharalimirjat@users.noreply.github.com>
2023-08-19 14:18:46 +0500

    Add files via upload

---
+++ IfelseStatement.py
@@ -0,0 +1,35 @@
+"""
+IF else Statement:
+    If the condition is true, the if block code is executed and
+    if the condition is false, the else block code is executed.
+
+Syntax:
+if condition:
+    # block of code if condition is True
+
+else:
+    # block of code if condition is False
+
+Example:
+"""
+# Example:01
+if True:
+    print("I run because if is true")
+else:
+    print("I run because if is false")
+
+# Example:02
+if False:
+    print("I run because if is true")
+else:
+    print("I run because if is false")
+
+
+# Example:03
+number = 10
+
+if number > 0:
+    print('Positive number')
+else:
+    print('Negative number')
+# OUTPUT: Positive number
