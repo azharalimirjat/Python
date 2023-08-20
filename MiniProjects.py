c4a4e4db972c5229f3b4a5a2727ee68615f2c0d9
Azhar Ali <99320098+azharalimirjat@users.noreply.github.com>
2023-08-20 13:46:35 +0500

    Add files via upload

---
+++ MiniProjects.py
@@ -0,0 +1,26 @@
+
+# MINI PROJECT:01
+marks = int(input('Enter your marks'))
+passing_marks = 50
+
+# METHOD:01
+# Direct check gives (False or True)
+# print(marks>passing_marks)
+
+
+# METHOD:02
+if marks>passing_marks:
+    print("Congratulations you passed")
+else:
+    print("You failed")
+
+
+
+# MINI PROJECT:02
+
+sound = input('what sound you like: ')
+
+if sound =='meow':
+    print("You Love Cats.  Cat Person")
+else:
+    print("You do not love cats")
